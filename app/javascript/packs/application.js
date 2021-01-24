6b1d135b5327f210cb95608ca5f4c2ce5a14729a
valentino320 <valentino320@gmail.com>
2021-01-24 12:32:35 +0000

    materialize nav added, fully working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,21 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import "materialize-css/dist/js/materialize"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// $( document ).load('turbolinks:load', function() {
+//   $(".dropdown-trigger").dropdown();
+//   alert('hey!');
+// } );
+
+$(function() {
+  $(".dropdown-trigger").dropdown();
+})
